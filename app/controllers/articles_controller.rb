e2fa431f45602afc1487ec7f31e945b42442bebc
Erick Carrillo <erickcarrillo8@gmail.com>
2016-07-20 06:57:18 +0000

    metodo show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController  < ApplicationController
     def index
         @articles = Article.all
     end 
+    #GET /articles/:id
+    def show
+        @articles = Article.find(params[:id])
+    end 
 end 
