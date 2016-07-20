75397cffd814161a54331935053200fc97507543
Erick Carrillo <erickcarrillo8@gmail.com>
2016-07-20 06:08:19 +0000

    articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+ 
+   resources :articles 
+  
+  # get "/articles"
+  # post "/articles"
+  # delete "/articles"
+  # get "/articles:id"
+  # get "/articles:new"
+  # get "/articles:id:edit"
+  # patch "/articles/:id"
+#   put "/articles/:id"
 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
