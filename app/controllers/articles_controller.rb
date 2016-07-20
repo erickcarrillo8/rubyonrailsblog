e99af262d2e34a94a109748abc675e98e2b390b2
Erick Carrillo <erickcarrillo8@gmail.com>
2016-07-20 07:33:50 +0000

    guardar en la base de datos desde formulario

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,18 @@ class ArticlesController  < ApplicationController
     def show
         @articles = Article.find(params[:id])
     end 
+    
+    #GET /articles/:new
+    def new
+        @article = Article.new
+    end 
+    #POST /articles
+    def create
+        @article = Article.new(title: params[:article][:title],
+                                body: params[:article][:body])
+                                
+        @article.save
+        redirect_to @article
+    end
+    
 end 
