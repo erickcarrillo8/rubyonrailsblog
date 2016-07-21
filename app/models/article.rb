4e9890908a285d77b66b1faee514a39022e36cd9
Erick Carrillo <erickcarrillo8@gmail.com>
2016-07-21 22:01:02 +0000

    Relaciones agregadas

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 	#La tabla => articles
 	#campos => article.title() => 'EL titulo de articulo'
 	#Escribir métodos.
-	
+	belongs_to :user
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: { minimum: 20}
 
